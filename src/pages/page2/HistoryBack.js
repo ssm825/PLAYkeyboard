48a6bdcd19a327ba7ce449dc88b33194e401bef3
seungyeon-rr <syryu926@gmail.com>
2022-10-06 13:38:42 +0900

    Add: page2 상단 구현

---
+++ src/pages/page2/HistoryBack.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HistoryBack = () => {
+
+   const navigate = useNavigate();
+
+   return (
+      <BackToPage onClick={() => navigate(-1)}>
+         <img src='/assets/images/ic_all_back.png' alt='back'/>
+      </BackToPage>
+   );
+};
+
+export default HistoryBack;
+
+const BackToPage = styled.button`
+   display:inline-block;
+   padding:20px;
+`;
