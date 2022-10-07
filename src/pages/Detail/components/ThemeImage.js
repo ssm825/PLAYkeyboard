f8a15e269b134dac81c5e3cc975a127d7e3900e1
youngsoon12 <enejwk789@gmail.com>
2022-10-07 09:06:00 +0900

    fixed : detail page 이미지보이는거 수정

--- src/pages/Detail/components/ThemeImage.js
+++ src/pages/Detail/components/ThemeImage.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 const ThemeImage = ({ themeData }) => {
+  const [copyData, setCopyData] = useState('');
+
+  useEffect(() => {
+    setCopyData({ ...themeData });
+  }, [themeData]);
+
   return (
-    <ImgArea slidesPerView={1}>
-      {themeData.images &&
-        themeData.images.map((data, idx) => {
-          return (
-            <SwiperSlide key={idx}>
-              <img src={data.imageUrl} alt={themeData.name} />
-            </SwiperSlide>
-          );
-        })}
-    </ImgArea>
+    <>
+      {copyData.images && (
+        <>
+          {themeData.images.length > 0 ? (
+            <ImgArea slidesPerView={1}>
+              {themeData.images.map((data, idx) => {
+                return (
+                  <SwiperSlide key={idx}>
+                    <img src={data.imageUrl} alt={themeData.name} />
+                  </SwiperSlide>
+                );
+              })}
+            </ImgArea>
+          ) : (
+            <ImgArea slidesPerView={1}>
+              <img src={copyData.imageUrl} alt="x"></img>
+            </ImgArea>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
